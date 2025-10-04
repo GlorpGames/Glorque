b550d2ecd0a1bfdccf49f7aa30628d50ce738146
Triston Caton <tristoncaton@outlook.com>
2025-10-04 00:20:02 -0400

    used ifdef instead of if defined(), whoops

--- Engine/source/console/codeBlock.cpp
+++ Engine/source/console/codeBlock.cpp
@@ -444,7 +444,7 @@ bool CodeBlock::read(StringTableEntry fileName, Stream &st)
       {
          U32 ip;
          st.read(&ip);
-#ifdef TORQUE_CPU_X64 || TORQUE_CPU_ARM64
+#if defined(TORQUE_CPU_X64) || defined(TORQUE_CPU_ARM64)
          *(U64*)(code + ip) = (U64)ste;
 #else
          code[ip] = *((U32 *)&ste);
